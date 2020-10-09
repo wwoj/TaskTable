b323d2611566a13a49a407c99c8808ff21d4cb57
wwoj <wojcik.wojtek@yahoo.pl>
2020-10-09 10:45:45 +0200

    Updates

---
+++ precache-manifest.b29e1dac946275f300d40c7237cca045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79aee20f457fb0afc5433c062423345",
+    "url": "/TaskTable/index.html"
+  },
+  {
+    "revision": "929e0bfc793a3c5e082d",
+    "url": "/TaskTable/static/css/main.ae05b843.chunk.css"
+  },
+  {
+    "revision": "7d732990f66795afd677",
+    "url": "/TaskTable/static/js/2.3666db9f.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/TaskTable/static/js/2.3666db9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929e0bfc793a3c5e082d",
+    "url": "/TaskTable/static/js/main.083874cb.chunk.js"
+  },
+  {
+    "revision": "79ca238af1efa6a59d08",
+    "url": "/TaskTable/static/js/runtime-main.44f07f26.js"
+  }
+]);
